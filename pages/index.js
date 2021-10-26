408678572ae3058d1d6413dc5a0cfb1e4aaafc28
Krupali09 <krupalitp@outlook.com>
2021-10-26 19:09:07 -0400

    minor change

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Hi I am Krupali. A Web Developer and content manager at Great Gulf]</p>
+        <p>[Hi I am Krupali Thakkar. A Web Developer and content manager at Great Gulf]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
