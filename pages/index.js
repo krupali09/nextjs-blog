471aa75354398c7461948abe7a2b383379f2f85b
[krupali] <[krupalitp@outlook.com]>
2022-01-21 14:23:19 -0500

    test

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Hi I am Krupali Thakkar. content manager at Great Gulf.</p>
+        <p>[Hi I am Krupali. content manager at Great Gulf.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
